bb94e949b843d018eb350086f0cfe445e15e9591
Renny Hong <rennyhong1103@gmail.com>
2022-07-08 13:09:37 -0700

    python scripts

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
+extern crate mailbox;
+
 use std::fs::File;
+use std::io::{BufReader, Read};
 
-fn main() => Result{
+use std::env;
+
+fn main() -> std::io::Result<()>{
     let file_name : &str = "res/test.mbox";
-    let fstream : File = File::open(file_name)?;
+    println!("{}",file_name);
 
+    let mut file : File = match File::open(file_name) {
+        Ok(f) => f,
+        Err(e) => panic!("Error {}",e),
+    };
+
+    let mbox = mailbox::read(file);
+
+    // for mail in mbox {
+    //     println!("{:?}", mail);
+    // }
+
+    //
+    // let mut buf_reader = BufReader::new(file);
+    // let mut contents : String = String::new();
+    //
+    // buf_reader.read_to_string(&mut contents)?;
+
+    // for line in &contents {
+    //     println!(line);
+    // }
+    // println!("{}",contents);
+
+
+    Ok(())
 }
