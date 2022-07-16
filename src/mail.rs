1a4b7fb8e9c0dace33e5d18ddccc6e69f7dc8633
Renny <rennyhong1103@gmail.com>
2022-07-15 19:22:05 -0700

    moved mail stuff to a seperate file

---
+++ src/mail.rs
@@ -0,0 +1,84 @@
+use std::fmt;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+pub struct Mail {
+    from: String,
+    to: String,
+    contents: String,
+}
+
+impl std::fmt::Display for Mail {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\n{}\n{}", self.from, self.to, self.contents)
+    }
+}
+
+impl Mail {
+    pub fn get_from(&self) -> &str {
+        &(self.from)
+    }
+    pub fn get_to(&self) -> &str {
+        &(self.to)
+    }
+    pub fn get_contents(&self) -> &str {
+        &(self.to)
+    }
+}
+
+pub fn parse_mbox(path: &str) -> Vec<Mail> {
+    let file : File = match File::open(path) {
+        Ok(f) => f,
+        Err(e) => panic!("Error {}",e),
+    };
+
+    let reader = BufReader::new(file);
+    let mut mail_unit: Mail = Mail {
+                                from: String::with_capacity(10),
+                                to: String::with_capacity(10),
+                                contents: String::new()
+    };
+    
+
+    let mut mails: Vec<Mail> = Vec::<Mail>::new();
+
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next() {
+        let l = line.unwrap();
+        let mut char_iter = l.char_indices();
+        
+        let mut first_four: String = String::with_capacity(20);
+        for _i in 0..4 {
+            if let Some(character) = char_iter.next() {
+                first_four += &(String::try_from(character.1).unwrap());
+            }
+        }
+
+        if first_four.len() >= 4 && &first_four == "From" {
+            if mail_unit.get_from().len() > 0 {
+                mails.push(mail_unit);
+                mail_unit = Mail {
+                    from: String::with_capacity(10),
+                    to: String::with_capacity(10),
+                    contents: String::new()
+                };
+            }
+
+            mail_unit.from = l;
+            match lines.next() {
+                Some(res) => mail_unit.to = res.unwrap(),
+                None => continue,
+            }
+        }else{
+            mail_unit.contents += &l;
+            mail_unit.contents += "\n";
+        }
+    }
+    if mail_unit.get_from().len() > 0 {
+        mails.push(mail_unit);
+    }
+
+    mails
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,44 @@
-extern crate mailbox;
+mod mail;
 
-use core::fmt;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-
-struct Mail {
-    from: String,
-    to: String,
-    contents: String,
-}
-
-impl fmt::Display for Mail {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}\n{}\n{}", self.from, self.to, self.contents)
-    }
-}
-
-impl Mail {
-    fn get_from(&self) -> &str {
-        &(self.from)
-    }
-    fn get_to(&self) -> &str {
-        &(self.to)
-    }
-    fn get_contents(&self) -> &str {
-        &(self.to)
-    }
-}
-
-fn parse_mbox(path: &str) -> Vec<Mail> {
-    let file : File = match File::open(path) {
-        Ok(f) => f,
-        Err(e) => panic!("Error {}",e),
-    };
-
-    let reader = BufReader::new(file);
-    let mut mail_unit: Mail = Mail {
-                                from: String::with_capacity(10),
-                                to: String::with_capacity(10),
-                                contents: String::new()
-    };
-    
-
-    let mut mails: Vec<Mail> = Vec::<Mail>::new();
-
-    let mut lines = reader.lines();
-
-    while let Some(line) = lines.next() {
-        let l = line.unwrap();
-
-        if l.len() >= 4 && &l[0..4] == "From" {
-            if mail_unit.get_from().len() > 0 {
-                mails.push(mail_unit);
-                mail_unit = Mail {
-                    from: String::with_capacity(10),
-                    to: String::with_capacity(10),
-                    contents: String::new()
-                };
-            }
-
-            mail_unit.from = l;
-            match lines.next() {
-                Some(res) => mail_unit.to = res.unwrap(),
-                None => continue,
-            }
-        }else{
-            mail_unit.contents += &l;
-            mail_unit.contents += "\n";
-        }
-    }
-    if mail_unit.get_from().len() > 0 {
-        mails.push(mail_unit);
-    }
-
-    mails
-}
+use std::env;
+use std::error::Error;
+use eml_parser::errors::EmlError;
+use eml_parser::parser::EmlParser;
 
 fn main() -> std::io::Result<()>{
-    let file_name : &str = "res/test.mbox";
-    println!("{}",file_name);
-    
-    let mails: Vec<Mail> = parse_mbox(file_name);
+    // let args: Vec<String> = env::args().collect();
+    //
+    // let file_name : &str = &args[1];
 
-    for mail in mails {
-        println!("{}", mail.get_from());
+    // let file_name : &str = "res/test.mbox";
+    // println!("{}",file_name);
+    // 
+    // let mails: Vec<mail::Mail> = mail::parse_mbox(file_name);
+    //
+    // // for mail in mails {
+    // //     println!("{}", mail.get_from());
+    // // }
+    //
+    //
+    // println!("{}", mails[0]);
+    // println!("______________________");
+    // println!("{}", mails[1]);
+    //
+    let file_name = "res/eml/■重要【ECOIS】水光熱運用異常 のお知らせ.eml";
+
+    let mut eml;
+    
+    match EmlParser::from_file(file_name) {
+        Ok(res) => eml = res,
+        Err(e) => panic!("{} error parsing file", e),
     }
+
+    if let Ok(parsed) = eml.parse() {
+        println!("{:?}", parsed.body);
+    } else {
+        println!("Failed to parse");
+    }
+
 
     Ok(())
 }
